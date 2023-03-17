76d5ffa091f2bd3f6875c8917c4a47d9103a787e
santra <moscowx1@mail.ru>
2023-03-17 21:28:44 +0500

    trying to salt passwords

--- lib/Test.hs
+++ lib/Test.hs
@@ -4,32 +4,30 @@
 
 module Test where
 
-import Data.ByteString (ByteString)
 import Crypto.Random.Entropy (getEntropy)
-import Crypto.KDF.PBKDF2 (fastPBKDF2_SHA512, Parameters (Parameters, iterCounts, outputLength))
-import Data.ByteArray (ByteArray(..), ByteArrayAccess(..))
-import Data.Functor((<&>))
-import Data.ByteString (pack)
+import Crypto.KDF.PBKDF2 (fastPBKDF2_SHA512, Parameters (..))
 import Data.Text (Text)
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-
-salt' :: ByteArray byteArray => Int -> IO byteArray
-salt' = getEntropy
+import Data.ByteString (ByteString)
+import Data.Text.Encoding (encodeUtf8)
 
 prm :: Parameters
-prm = Parameters { iterCounts = 4000, outputLength = 50 }
+prm = Parameters { iterCounts = 8192, outputLength = 64 }
 
-t2 :: (ByteArrayAccess password, ByteArray out, ByteArray bt) => password -> IO bt -> IO out
-t2 pwd s = s <&> fastPBKDF2_SHA512 prm pwd
+t3 :: Text -> IO (ByteString, ByteString)
+t3 pwd = let salt = getEntropy 32
+             pwdB = encodeUtf8 pwd
+  in (,) <$> salt <*> (fastPBKDF2_SHA512 prm pwdB <$> salt)
 
-m :: (forall o. ByteArray o) => IO out
-m = t2 pwd' (salt' 30)
-  where
-    pwd' :: ByteString
-    pwd' = "hi"
-
---saltPwd :: Text -> IO (Text, Text)
---saltPwd pwd = do
---  let pwd' = encodeUtf8 pwd
---  s <- salt' 1
---  pure ("a", "b")
+-- working
+t :: Text -> IO Bool
+t pwd = do
+  ss <- getEntropy 32 :: IO ByteString
+  let k1  = fastPBKDF2_SHA512 prm (encodeUtf8 pwd) ss :: ByteString
+  let k2 =  fastPBKDF2_SHA512 prm (encodeUtf8 pwd) ss
+  pure $ k1 == k2
+-- not working
+k :: Text -> IO Bool
+k pwd = do
+  (salt, hp1) <- t3 pwd
+  let hp2 = fastPBKDF2_SHA512 prm (encodeUtf8 pwd) salt
+  pure $ hp1 == hp2
