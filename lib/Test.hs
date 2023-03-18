cb303f697e79e23aaf775b31c2ab15a095b76a31
santra <moscowx1@mail.ru>
2023-03-18 18:10:49 +0500

    refactor draft for auth

--- lib/Test.hs
+++ lib/Test.hs
@@ -5,7 +5,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Test where
 
@@ -30,83 +30,81 @@ import Control.Monad.Reader (ReaderT, MonadIO (liftIO), guard, MonadTrans (lift)
 import Data.Time (getCurrentTime, UTCTime)
 import Debug.Trace (traceShow)
 
-prm :: Parameters
-prm = Parameters { iterCounts = 8192, outputLength = 64 }
-
-t3 :: ByteString -> IO (ByteString, ByteString)
-t3 pwd = let salt = getEntropy 32
-  in (,) <$> salt <*> (fastPBKDF2_SHA512 prm pwd <$> salt)
-
--- working
-t :: ByteString -> IO Bool
-t pwd = do
-  ss <- getEntropy 32 :: IO ByteString
-  let k1  = fastPBKDF2_SHA512 prm pwd ss :: ByteString
-  let k2 =  fastPBKDF2_SHA512 prm pwd ss
-  pure $ k1 == k2
--- not working
-k :: ByteString -> IO Bool
-k pwd = do
-  (salt, hp1) <- t3 pwd
-  let hp2 = fastPBKDF2_SHA512 prm pwd salt
-  pure $ hp1 == hp2
-
-createUser
-  :: (ReaderT SqlBackend (NoLoggingT IO) a -> NoLoggingT IO a)
-  -> Text
-  -> Text
-  -> User
-createUser _ _ _ = do
-  undefined
-
-getAll_ :: ReaderT SqlBackend (NoLoggingT IO) ()
-getAll_ = do
-  us <- getAll
-  liftIO $ print us
-  pure ()
-
-getAll :: ReaderT SqlBackend (NoLoggingT IO) [E.Entity User]
-getAll = E.select $ E.from $ E.table @User
-
 password :: ByteString
 password = "Asasdfbgzxc;lnmksda"
 
 loginText :: Text
 loginText = "santrsasda"
 
-insertUser2 = insertUser2' loginText password
+initHandle = Handle
+  { generatingIterCount = 8192
+  , hashedPasswordLength = 64
+  , saltLength = 32
+  }
 
-insertUser2' :: Text -> ByteString -> ReaderT SqlBackend (NoLoggingT IO) (E.Key User)
-insertUser2' login pwd = do
-  salt <- liftIO (getEntropy 32 :: IO ByteString)
-  let pwdHashed = fastPBKDF2_SHA512 prm pwd salt
+data Handle = Handle
+  { generatingIterCount :: Int
+  , hashedPasswordLength :: Int
+  , saltLength :: Int
+  }
+
+type Password = ByteString
+type Salt = ByteString
+type Login = Text
+type HashedPassword = ByteString
+
+getHashedPwd :: Handle -> Password -> Salt -> HashedPassword
+getHashedPwd Handle{..} = fastPBKDF2_SHA512 prms
+  where
+    prms = Parameters
+      { iterCounts = generatingIterCount
+      , outputLength = hashedPasswordLength
+      }
+
+createUser :: MonadIO m => Handle -> Login -> Password -> m User
+createUser h@Handle{..} login pwd = do
+  (salt :: ByteString) <- liftIO $ getEntropy saltLength
   curTime <- liftIO getCurrentTime
-  E.insert $ User
+  let pwdHashed = getHashedPwd h pwd salt
+  pure $ User
     { userLogin = login
     , userSalt = salt
     , userPassword = pwdHashed
     , userCreatedAt = curTime
     }
 
-userByLogin :: Text -> ReaderT SqlBackend (NoLoggingT IO) (Maybe (E.Entity User))
+register 
+  :: MonadIO m 
+  => Handle 
+  -> Login
+  -> Password
+  -> ReaderT SqlBackend m Bool
+register h login password = do
+  user <- liftIO $ createUser h login password
+  res <- E.insertUniqueEntity user
+  pure $ case res of
+    Nothing -> False
+    Just _ -> True
+
+userByLogin 
+  :: (MonadIO m) 
+  => Text 
+  -> ReaderT SqlBackend m (Maybe (E.Entity User))
 userByLogin login = E.selectOne $ do
   user <- E.from $ E.table @User
   E.where_ $ user ^. UserLogin ==. E.val login
   pure user
 
-login :: Text -> ByteString -> ReaderT SqlBackend (NoLoggingT IO) (Maybe (E.Entity User))
-login login password = do
-  Just user <- userByLogin login
-  let hashedPwd = fastPBKDF2_SHA512 prm password (userSalt $ E.entityVal user)
-  pure $ if hashedPwd == userPassword (E.entityVal user)
-    then Just user
-    else Nothing
-
-login' :: ReaderT SqlBackend (NoLoggingT IO) ()
-login' = do
-  u <- login loginText password
-  liftIO $ print u
-  pure ()
+login 
+  :: (MonadIO m) 
+  => Handle 
+  -> Login 
+  -> Password 
+  -> ReaderT SqlBackend m Bool
+login h login password = do
+  user <- userByLogin login
+  let hashed = getHashedPwd h password 
+  pure True
 
 truncateUsers :: ReaderT SqlBackend (NoLoggingT IO) ()
 truncateUsers = E.rawExecute "truncate table \"user\" cascade;" []
@@ -115,9 +113,11 @@ main :: IO ()
 main = do
   Config {..} <- throwLeft <$> eitherDecodeFileStrict "config.json"
   runNoLoggingT do
-    withPostgresqlPool (encodeUtf8 _connectionString) _poolConnections \pool -> do
-      --runSqlPool truncateUsers pool
+    withPostgresqlPool 
+      (encodeUtf8 _connectionString) 
+      _poolConnections \pool -> do 
+        runSqlPool truncateUsers pool
       --runSqlPool (void insertUser2) pool
       --runSqlPool (void insertUser) pool
-      runSqlPool login' pool
+      --runSqlPool login' pool
       --runSqlPool getAll_ pool
