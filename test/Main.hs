ec885cf41c745cbfade8f519b4b77cd41ca3b3f2
santra <moscowx1@mail.ru>
2023-03-19 16:11:45 +0500

    test for auth

--- test/Main.hs
+++ test/Main.hs
@@ -3,60 +3,53 @@
 
 module Main where
 
-import Test.Tasty (testGroup, defaultMain, TestTree)
-import Test.Tasty.HUnit (testCase, (@?=))
-import qualified Logic.Auth as Auth
-import Logic.Auth (Config(..), Handle(..), login, register)
-import Data.Functor.Identity (Identity)
+import Control.Monad.State (StateT (runStateT), modify, get)
+import Data.Functor.Identity (Identity (Identity))
+import Data.List (find)
 import Data.Text.Encoding (encodeUtf8)
 import Data.Time (UTCTime(..), secondsToDiffTime)
 import Data.Time.Calendar.OrdinalDate (fromOrdinalDate)
 import DataAccess.Data (User (userLogin, User))
-import Data.List (find)
-import Types (Login)
-import Debug.Trace (traceShow)
+import qualified Logic.Auth as Auth
+import Logic.Auth (Config(..), Handle(..), login, register)
+import Test.Tasty (testGroup, defaultMain, TestTree)
+import Test.Tasty.HUnit (testCase, (@?=))
 
 main :: IO ()
 main = defaultMain $ testGroup "" [authTest]
 
-
-config :: Auth.Config
-config = Auth.Config
-  { generatingIterCount = 2000
-  , hashedPasswordLength = 60
-  , saltLength = 20
-  }
-
-db :: [User]
-db = []
-
-add :: User -> [User]
-add = (: db)
-
-get :: Login -> Maybe User
-get login' = find (\User{..} -> userLogin == login') db
-
-handle :: Auth.Handle Identity
-handle = Auth.Handle
-  { _config = config
+handle :: Auth.Handle ((StateT [User]) Identity)
+handle = Handle
+  { _config = Auth.Config
+      { generatingIterCount = 2000
+      , hashedPasswordLength = 60
+      , saltLength = 20
+      }
   , _generateSalt = (const . pure . encodeUtf8) "azjkl;sdf"
   , _currentTime = pure $ UTCTime
-    { utctDay = fromOrdinalDate 2022 1
-    , utctDayTime = secondsToDiffTime 12
-    }
+      { utctDay = fromOrdinalDate 2022 1
+      , utctDayTime = secondsToDiffTime 12
+      }
   , _addToDb = \user -> do
-     _ <- pure $ add user
-     pure $ Just user
-  , _getUser = pure . get
+      same <- sameLoginUser (userLogin user)
+      case same of
+        Nothing -> modify (user:) >> pure (Just user)
+        Just _ -> pure Nothing
+  , _getUser = \login' -> do
+      sameLoginUser login'
   }
+    where
+      sameLoginUser login' = do
+        users <- get
+        let sameLogin = find (\User{..} -> userLogin == login') users
+        pure sameLogin
 
 authTest :: TestTree
 authTest = testGroup "Auth logic tests"
-  [ testCase "register login test" $ do
+  [ testCase "logging after registering test" $ do
       let user = "AsdaSdzxcv"
       let password = "asdfgsdf"
-      let regRes = register handle user password
-      let _ = traceShow db ()
-      let logRes = login handle user password
-      logRes @?= regRes
+      let Identity (reg, db) = runStateT (register handle user password) []
+      let Identity (log', _) = runStateT (login handle user password) db
+      reg @?= log'
   ]
