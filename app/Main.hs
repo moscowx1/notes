4760a789c0213eab0697b4fcf708e177c650196b
santra <moscowx1@mail.ru>
2023-03-19 19:44:34 +0500

    refactor

--- app/Main.hs
+++ app/Main.hs
@@ -6,32 +6,39 @@ module Main where
 
 import Api (Api(..), UserApi (..))
 import Config (Config(..))
-import Control.Monad.Reader (ReaderT)
 import Control.Monad.Logger (NoLoggingT(runNoLoggingT))
 import Control.Monad.IO.Class (liftIO)
 import Data.Aeson (eitherDecodeFileStrict)
-import Database.Persist.Postgresql (withPostgresqlPool, runSqlPool, runMigration, SqlBackend)
+import Database.Persist.Postgresql 
+  ( withPostgresqlPool
+  , runSqlPool
+  , runMigration
+  )
 import Data.Text.Encoding (encodeUtf8)
 import DataAccess.Data (migrateAll)
 import Network.Wai.Handler.Warp (run)
+import Types(SqlRuner)
 import Servant.Server.Generic (genericServeT)
 import Servant (Application)
 import Utils (throwLeft)
 
 main :: IO ()
 main = do
-  Config {..} <- throwLeft <$> eitherDecodeFileStrict "config.json"
-  runNoLoggingT do
-    withPostgresqlPool
+  c <- throwLeft <$> eitherDecodeFileStrict "config.json"
+  getPool c \pool -> do
+      runSqlPool (runMigration migrateAll) pool
+      runServer c pool
+
+  where
+    getPool Config{..} = runNoLoggingT . withPostgresqlPool
       (encodeUtf8 _connectionString)
       _poolConnections
-      \pool -> do
-        runSqlPool (runMigration migrateAll) pool
-        liftIO $ run _port (server (runNoLoggingT . (`runSqlPool` pool)))
 
+    runServer Config {..} pool = liftIO $
+      run _port (server (runNoLoggingT . (`runSqlPool` pool)))
 
 server 
-  :: (forall a. ReaderT SqlBackend (NoLoggingT IO) a -> IO a) 
+  :: (forall a. SqlRuner a)
   -> Application
 server _ = genericServeT liftIO
   Api
