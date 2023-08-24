417a7b74da84e5439d1354ed6fe3fb455086d0df
moscowx1 <moscowx1@mail.ru>
2023-08-24 20:24:42 +0500

    create pool

--- back/app/Main.hs
+++ back/app/Main.hs
@@ -2,27 +2,40 @@
 
 module Main (main) where
 
+import qualified Config.Config as GC
+import qualified Config.Database as DC
 import Data.Aeson (eitherDecodeFileStrict)
-import Config.Config (Config)
+import Data.Pool (PoolConfig, defaultPoolConfig, newPool)
+import Database.Beam.Postgres (ConnectInfo (..), Connection, close, connect)
 
-{-
-conInfo :: ConnectInfo
-conInfo =
-  ConnectInfo
-    { connectHost = "localhost"
-    , connectPort = 5432
-    , connectUser = "postgres"
-    , connectPassword = "postgres"
-    , connectDatabase = "notes"
-    }
--}
+readConfig :: IO GC.Config
+readConfig =
+  eitherDecodeFileStrict "config.json" >>= \case
+    Left e -> error e
+    Right x -> pure x
 
-readConfig :: IO Config
-readConfig = eitherDecodeFileStrict "config.json" >>= \case
-      Left e -> error e
-      Right x -> pure x
+createPool ::
+  GC.Config ->
+  PoolConfig Connection
+createPool config =
+  defaultPoolConfig
+    (connect connectInfo)
+    close
+    (DC._keepAliveUnusedConnection dbConfig)
+    (DC._maxConnections dbConfig)
+ where
+  dbConfig = GC._database config
+  connectInfo =
+    ConnectInfo
+      { connectHost = DC._host dbConfig
+      , connectPort = DC._port dbConfig
+      , connectUser = DC._user dbConfig
+      , connectPassword = DC._password dbConfig
+      , connectDatabase = DC._database dbConfig
+      }
 
 main :: IO ()
 main = do
   config <- readConfig
-  print config
+  _ <- newPool $ createPool config
+  print "end"
--- back/lib/Config/Database.hs
+++ back/lib/Config/Database.hs
@@ -6,15 +6,15 @@ module Config.Database where
 
 import Data.Aeson (defaultOptions)
 import Data.Aeson.TH (constructorTagModifier, deriveJSON, fieldLabelModifier)
-import Data.Text (Text)
+import Data.Word (Word16)
 import GHC.Generics (Generic)
 
 data Config = Config
-  { _host :: Text
-  , _port :: Int
-  , _user :: Text
-  , _password :: Text
-  , _database :: Text
+  { _host :: String
+  , _port :: Word16
+  , _user :: String
+  , _password :: String
+  , _database :: String
   , _maxConnections :: Int
   , _keepAliveUnusedConnection :: Double
   }
